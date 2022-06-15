c1c1c57ba8754965cca99ffcb3c45735ed01aaab
kihyun1998 <kihtun1998@naver.com>
2022-06-15 13:31:16 +0900

    Add Full weathers's Icons

--- javascript/icon.js
+++ javascript/icon.js
@@ -3,7 +3,7 @@ function getIcon(weather){
     if(weather=="Clear"){
         return "./image/sunny.png";
     }
-    else if(weather=="Clouds"||weather=="Mist" || weather=="Fog"){
+    else if(weather=="Clouds"||weather=="Mist" || weather=="Fog" || weather=="Haze"){
         return "./image/cloudy.png";
     }
     else if(weather=="Rain"){
@@ -14,5 +14,20 @@ function getIcon(weather){
     }
     else if(weather=="Drizzle"){
         return "./image/drop.png";
+    }
+    else if(weather=="Dust" || weather=='Sand'){
+        return "./image/dust.png";
+    }
+    else if(weather=='Thunderstorm'){
+        return "./image/storm.png";
+    }
+    else if(weather=='Tornado'){
+        return "./image/tornado.png";
+    }
+    else if(weather=='Ash'){
+        return "./image/volcano.png";
+    }
+    else if(weather=='Squall'){
+        return "./image/squall.png";
     }
 }
