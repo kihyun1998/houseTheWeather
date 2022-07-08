27e64e3830f6041a5f497e5f06b67a162cd3b862
naman <kihyun1998@naver.com>
2022-07-08 18:42:02 +0900

    delete air pollution UI

--- javascript/geoChart.js
+++ javascript/geoChart.js
@@ -3,7 +3,6 @@ let lon = window.localStorage.getItem('lon');
 
 apiLoc(lat,lon);
 apiGraph(lat,lon);
-apiairChart(lat,lon)
 
 setDate("dateText");
 
--- javascript/geoHome.js
+++ javascript/geoHome.js
@@ -1,7 +1,6 @@
 let lat = window.localStorage.getItem('lat');
 let lon = window.localStorage.getItem('lon');
 
-apiAir(lat,lon);
 apiLoc(lat,lon);
 apiHome(lat,lon);
 
